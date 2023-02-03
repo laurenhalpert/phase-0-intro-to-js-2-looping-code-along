44b07493d50e1b4b224f133888eb3d9a604cd621
Lauren <halpertln@gmail.com>
2023-02-03 15:07:45 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+    //console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, event){
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+        
+    }
+    return newArray;
+}
+
+function countDown(integer) {
+    
+    while (integer>=0) {
+        console.log(integer);
+        integer--;
+    }
+}
